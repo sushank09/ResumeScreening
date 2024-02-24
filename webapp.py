533322b5f060b27b4bf71b9f17bf2879d909e625
Big Bright Brothers <145913327+BigBrightBrothers@users.noreply.github.com>
2024-02-24 12:00:07 +0530

    Update webapp.py v1.8

--- webapp.py
+++ webapp.py
@@ -3,13 +3,13 @@ import PyPDF2
 import pdfplumber
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("Candidate Selection Tool")
 
 st.subheader("NLP Based Resume Screening")
 
-st.caption("Aim of this project is to check whether a candidate is qualified for a role based his or her education, experience, and other information captured on their resume. In a nutshell, it's a form of pattern matching between a job's requirements and the qualifications of a candidate based on their resume.")
+st.caption("Aim of this project is to check whether a candidate is qualified for a role based on his or her education, experience, and other information captured on their resume. In a nutshell, it's a form of pattern matching between a job's requirements and the qualifications of a candidate based on their resume.")
 
 uploadedJD = st.file_uploader("Upload Job Description", type="pdf")
 
@@ -71,9 +71,14 @@ if click:
     keyword_counts = {}
     for keyword in matching_keywords:
         keyword_counts[keyword] = resume.lower().count(keyword.lower())
-    df = pd.DataFrame.from_dict(keyword_counts, orient='index', columns=['Count'])
     
     # Plotting bar chart
-    st.bar_chart(df)
+    plt.figure(figsize=(10,6))
+    plt.bar(keyword_counts.keys(), keyword_counts.values())
+    plt.xlabel('Keywords')
+    plt.ylabel('Count')
+    plt.title('Matching Keywords Count')
+    plt.xticks(rotation=45, ha='right')
+    st.pyplot(plt)
 
 st.caption(" ~ made by siddhraj")
